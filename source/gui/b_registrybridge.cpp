ac4097c17c8b1159c75f0ee130b04335c8d6624a
麻菜 <yuri.young@qq.com>
2026-09-15 01:47:16 +0800

    gui: RegistryBridge watchSelection + selectionChanged convenience

--- source/gui/b_registrybridge.cpp
+++ source/gui/b_registrybridge.cpp
@@ -1,5 +1,7 @@
 #include "gui/b_registrybridge.h"
 
+#include <bakuon/core/Components.h>
+
 namespace bakuon::gui {
 
 RegistryBridge::RegistryBridge(core::Registry& registry, QObject* parent)
@@ -9,5 +11,26 @@ RegistryBridge::RegistryBridge(core::Registry& registry, QObject* parent)
 }
 
 RegistryBridge::~RegistryBridge() = default;
+
+void RegistryBridge::watchSelection(const QString& tag)
+{
+    using bakuon::core::components::Selected;
+
+    m_connections.push_back(m_registry.onConstruct<Selected>(
+        [this, tag](core::Registry&, core::Handle id) {
+            Q_EMIT entityConstructed(tag, id);
+            Q_EMIT selectionChanged();
+        }));
+    m_connections.push_back(m_registry.onUpdate<Selected>(
+        [this, tag](core::Registry&, core::Handle id) {
+            Q_EMIT entityUpdated(tag, id);
+            Q_EMIT selectionChanged();
+        }));
+    m_connections.push_back(m_registry.onDestroy<Selected>(
+        [this, tag](core::Registry&, core::Handle id) {
+            Q_EMIT entityDestroyed(tag, id);
+            Q_EMIT selectionChanged();
+        }));
+}
 
 } // namespace bakuon::gui
